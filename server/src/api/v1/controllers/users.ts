c056938feedcd33088f5da69a6975a224a831ae4
taegan <warretaega@gmail.com>
2021-06-06 17:53:46 -0700

    Added server side validation for registration

--- server/src/api/v1/controllers/users.ts
+++ server/src/api/v1/controllers/users.ts
@@ -1,13 +1,47 @@
 import { Request, Response } from 'express';
+import validator from 'validator';
 
-// TODO: Implement password policy
-// maybe... idk such a simple app that doesnt really matter
+interface userRegistration {
+    username: string,
+    password: string,
+    confirmedPassword: string
+}
+
+interface Errors {
+    [key: string]: any
+}
+
+const validate_registration = (data: userRegistration) => {
+    let errors: Errors = {};
+    if (validator.isEmpty(data.username) || !validator.isAlphanumeric(data.username)) {
+        errors.username = 'Username must only contains letters and numbers';
+    }
+    if (validator.isEmpty(data.password) || !validator.isStrongPassword(data.password)) {
+        errors.password = 'Password must be at least 8 characters and contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 symbol.';
+    }
+    if (validator.isEmpty(data.confirmedPassword) || !validator.isStrongPassword(data.confirmedPassword)) {
+        errors.confirmedPassword = 'Password must be at least 8 characters and contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 symbol.';
+    }
+    if (!validator.equals(data.password, data.confirmedPassword)) {
+        errors.mismatched = 'Passwords must match';
+    }
+    return {
+        errors,
+        isValid: Object.keys(errors).length > 1 ? false : true
+    }
+}
 
 const register_user = async (req: Request, res: Response) => {
     try {
-        console.log(req.body);
+        const { errors, isValid } = validate_registration(req.body);
+        if (!isValid) {
+            res.status(400).send(errors);
+            return;
+        }
         const { username, password, confirmedPassword } = req.body;
-        // TODO: correct http code
+        console.log(username);
+        console.log(password);
+        console.log(confirmedPassword);
         res.status(200).send();
     } catch (e) {
         console.log(e);
